a63c60d4f254c1b266aef2af1ef579f4c3436c33
Michiel <m.tebraake.95@gmail.com>
2022-05-02 15:10:58 +0200

    Fix RPM plugin implementation

--- mavros_extras/src/plugins/rpm.cpp
+++ mavros_extras/src/plugins/rpm.cpp
@@ -1,3 +1,11 @@
+/**
+ * @brief RPM plugin
+ * @file rpm.cpp
+ * @author Michiel te Braake <m.j.m.tebraake@gmail.com>
+ *
+ * @addtogroup plugin
+ * @{
+ */
 /*
  * Copyright 2022 Michiel te Braake.
  *
@@ -5,83 +13,62 @@
  * in the top-level LICENSE file of the mavros repository.
  * https://github.com/mavlink/mavros/tree/master/LICENSE.md
  */
+
+#include <mavros/mavros_plugin.h>
+#include <eigen_conversions/eigen_msg.h>
+
+#include <mavros_msgs/RPM.h>
+
+namespace mavros {
+namespace extra_plugins {
 /**
  * @brief RPM plugin
- * @file rpm.cpp
- * @author Michiel te Braake
  *
- * @addtogroup plugin
- * @{
+ * This plugin is intended to publish MAV rotor RPM from FCU.
  */
-
-#include <tf2_eigen/tf2_eigen.h>
-
-#include <string>
-
-#include "rcpputils/asserts.hpp"
-#include "mavros/mavros_uas.hpp"
-#include "mavros/plugin.hpp"
-#include "mavros/plugin_filter.hpp"
-
-#include "mavros_msgs/msg/vibration.hpp"
-
-namespace mavros
-{
-namespace extra_plugins
-{
-using namespace std::placeholders;      // NOLINT
-
-/**
- * @brief RPM plugin
- * @plugin RPM
- *
- * This plugin is intended to publish RAW_RPM message.
- */
-class RPMPlugin : public plugin::Plugin
-{
+class RPMPlugin : public plugin::PluginBase {
 public:
-  explicit RPMPlugin(plugin::UASPtr uas_)
-  : Plugin(uas_, "rpm")
-  {
-    enable_node_watch_parameters();
+	RPMPlugin() : PluginBase(),
+		rpm_nh("~rpm")
+	{ }
 
-    node_declate_and_watch_parameter(
-      "frame_id", "base_link", [&](const rclcpp::Parameter & p) {
-        frame_id = p.as_string();
-      });
+	void initialize(UAS &uas_) override
+	{
+		PluginBase::initialize(uas_);
 
-    rpm_pub = node->create_publisher<mavros_msgs::msg::RPM>("~/rpm", 10);
-  }
+		rpm_nh.param<std::string>("frame_id", frame_id, "base_link");
 
-  Subscriptions get_subscriptions() override
-  {
-    return {
-      make_handler(&RPMPlugin::handle_rpm)
-    };
-  }
+		rpm_pub = rpm_nh.advertise<mavros_msgs::RPM>("rpm", 10);
+	}
+
+	Subscriptions get_subscriptions() override
+	{
+		return {
+			make_handler(&RPMPlugin::handle_rpm)
+		};
+	}
 
 private:
-  rclcpp::Publisher<mavros_msgs::msg::RPM>::SharedPtr vibration_pub;
+	ros::NodeHandle rpm_nh;
 
-  std::string frame_id;
+	std::string frame_id;
 
-  void handle_rpm(
-    const mavlink::mavlink_message_t * msg [[maybe_unused]],
-    mavlink::common::msg::RAW_RPM & rpm,
-    plugin::filter::SystemAndOk filter [[maybe_unused]])
-  {
-    auto rpm_msg = mavros_msgs::msg::RPM();
+	ros::Publisher rpm_pub;
 
-    rpm_msg.header = uas->synchronized_header(frame_id, rpm.time_usec);
+	void handle_rpm(const mavlink::mavlink_message_t *msg, mavlink::common::msg::RAW_RPM &raw_rpm)
+	{
+		auto rpm_msg = boost::make_shared<mavros_msgs::RPM>();
 
-    rpm_msg.index = rpm.index;
-    rpm_msg.frequency = rpm.frequency;
+		rpm_msg->header.stamp = ros::Time::now();
 
-    rpm_pub->publish(rpm_msg);
-  }
+    rpm_msg->index = raw_rpm.index;
+    rpm_msg->frequency = raw_rpm.frequency;
+
+		rpm_pub.publish(rpm_msg);
+	}
 };
-}       // namespace extra_plugins
-}       // namespace mavros
+}	// namespace extra_plugins
+}	// namespace mavros
 
-#include <mavros/mavros_plugin_register_macro.hpp>  // NOLINT
-MAVROS_PLUGIN_REGISTER(mavros::extra_plugins::VibrationPlugin)
+#include <pluginlib/class_list_macros.h>
+PLUGINLIB_EXPORT_CLASS(mavros::extra_plugins::RPMPlugin, mavros::plugin::PluginBase)
